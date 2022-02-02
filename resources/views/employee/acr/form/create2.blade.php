b5a3b4f6d5f7dd8318c9691f2ec39e10bd7468b2
manojpanwar000 <manojpanwar000@yahoo.co.in>
2022-02-02 10:38:46 +0530

    form 2

--- resources/views/employee/acr/form/create2.blade.php
+++ resources/views/employee/acr/form/create2.blade.php
@@ -19,22 +19,26 @@
 			@csrf
 			<input type="hidden" name="acr_id" value='{{$acr->id}}'>
 			<div class="form-group">
-			  <label for="good_work" class="fw-bold h4">
-			  	2- Exceptionally good works done, if any, apart from routine duties during the period of appraisal (Max. 100 Words)
-			  </label>
-			  <textarea class="form-control rounded-0" id="good_work"  name="good_work" rows="5">
-			  		{{$acr->good_work??''}}
-			  </textarea>
-			  <label for="difficultie" class="fw-bold h4">
-			  	3- Difficulties faced in performing the assigned 'Tasks/Duties' (Max. 100 Words)
-			  </label>
-			  <textarea class="form-control rounded-0" id="difficultie"  name="difficultie" rows="5">
-			  		{{$acr->difficultie??''}}
-			  </textarea>
+				<label for="good_work" class="fw-bold h4">
+				  	2- Exceptionally good works done, if any, apart from routine duties during the period of appraisal (Max. 100 Words)
+				</label>
+				<textarea class="form-control rounded-0" id="good_work"  name="good_work" rows="3"
+				  	@if(!empty($acr->good_work))
+						style="background-color:#F0FFF0;"
+					@endif
+				>{{$acr->good_work??''}}</textarea>
+			  	<label for="difficultie" class="fw-bold h4">
+			  		3- Difficulties faced in performing the assigned 'Tasks/Duties' (Max. 100 Words)
+			  	</label>
+			  	<textarea class="form-control rounded-0" id="difficultie"  name="difficultie" rows="3"
+			  	@if(!empty($acr->difficultie))
+						style="background-color:#F0FFF0;"
+					@endif
+				>{{$acr->difficultie??''}}</textarea>
 			</div>
 
-		<div class="form-group mt-2">
-	        <button type="submit" class="btn btn-primary " >Save
+		<div class="form-group mt-2 text-end">
+	        <button type="submit" class="btn btn-primary">Save
 	    </div>
 		</form>
 	</div>
