72b776c3af321e91ef2a49bef022c991408e0a17
manojpanwar000 <manojpanwar000@yahoo.co.in>
2022-02-07 13:53:36 +0530

    page2

--- resources/views/employee/acr/form/create2.blade.php
+++ resources/views/employee/acr/form/create2.blade.php
@@ -6,7 +6,9 @@
 	Part -II Self-Appraisal <small>Page -2</small>
 @endsection
 @section('content')
-	@include('employee.acr.form._formHeader',['acr'=>$acr])
+	@if(!$view)
+		@include('employee.acr.form._formHeader',['acr'=>$acr])
+	@endif
 	<div class="card">
 		<div class="card-body">
 			<form class="form-horizontal" method="POST" action="{{route('acr.form.store2')}}">
@@ -16,24 +18,33 @@
 					<label for="good_work" class="fw-bold h5">
 					  	2- Exceptionally good works done, if any, apart from routine duties during the period of appraisal (Max. 100 Words)
 					</label>
-					<textarea class="form-control rounded-0" id="good_work"  name="good_work" rows="3"
-					  	@if(!empty($acr->good_work))
-							style="background-color:#F0FFF0;"
-						@endif
-					>{{$acr->good_work??''}}</textarea>
+					@if(!$view)
+						<textarea class="form-control rounded-0" id="good_work"  name="good_work" rows="3"
+						  	@if(!empty($acr->good_work))
+								style="background-color:#F0FFF0;"
+							@endif
+						>{{$acr->good_work??''}}</textarea>
+					@else
+						<p class="text-info">{{$acr->good_work??'--no data filled--'}}</p>
+					@endif
 				  	<label for="difficultie" class="fw-bold h5">
 				  		3- Difficulties faced in performing the assigned 'Tasks/Duties' (Max. 100 Words)
 				  	</label>
-				  	<textarea class="form-control rounded-0" id="difficultie"  name="difficultie" rows="3"
-				  	@if(!empty($acr->difficultie))
-							style="background-color:#F0FFF0;"
-						@endif
-					>{{$acr->difficultie??''}}</textarea>
+				  	@if(!$view)
+					  	<textarea class="form-control rounded-0" id="difficultie"  name="difficultie" rows="3"
+					  	@if(!empty($acr->difficultie))
+								style="background-color:#F0FFF0;"
+							@endif
+						>{{$acr->difficultie??''}}</textarea>
+				  	@else
+				  		<p class="text-info">{{$acr->difficultie??'--no data filled--'}}</p>
+				  	@endif
 				</div>
-
-			<div class="form-group mt-2 text-end">
-		        <button type="submit" class="btn btn-primary">Save
-		    </div>
+			@if(!$view)
+				<div class="form-group mt-2 text-end">
+			        <button type="submit" class="btn btn-primary">Save
+			    </div>
+		    @endif
 			</form>
 		</div>
 	</div>
