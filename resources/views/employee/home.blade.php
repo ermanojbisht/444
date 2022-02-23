05b35999dc385924cf3cbaefddd4975811c765c1
manojpanwar000 <manojpanwar000@yahoo.co.in>
2022-02-23 16:53:47 +0530

    dashboard

--- resources/views/employee/home.blade.php
+++ resources/views/employee/home.blade.php
@@ -56,12 +56,35 @@
                     @endif
                     @if(strpos(auth()->user()->email, 'emp.com') == false && auth()->user()->email_verified_at)
                     <div class="row p-3">
-                        <div class="col-md-3">
+                        @if($user_grievance_count)
+                            <div class="col-lg-3 col-md-4 col-sm-12 ">
+                                <div class="card">
+                                    <div class="card-body ">
+                                        <div class="row">
+                                            <div class="col-5">
+                                                <p class="display-2 font-weight-bold">
+                                                {{$user_grievance_count}}
+                                                </p>
+                                            </div>
+                                            <div class="col-7 d-flex align-items-center">
+                                                <p class="h4 text-center">Grievance</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="card-footer text-right text-decoration-none bg-light  " href="{{route('employee.hr_grievance')}}">
+                                        <span>view</span>
+                                        <i class="fa fa-arrow-circle-right" ></i>
+                                    </a>
+                                </div>
+                            </div>
+                        @endif
+                        @if($user_acr_count)
+                        <div class="col-lg-3 col-md-4 col-sm-12 ">
                             <div class="card">
                                 <div class="card-body ">
                                     <div class="row">
                                         <div class="col-5">
-                                            <p class="display-3 font-weight-bold">
+                                            <p class="display-2 font-weight-bold">
                                             {{$user_acr_count}}
                                             </p>
                                         </div>
@@ -70,8 +93,79 @@
                                         </div>
                                     </div>
                                 </div>
+                                <a class="card-footer text-right text-decoration-none bg-light  " href="{{route('acr.myacrs')}}">
+                                    <span>view</span>
+                                    <i class="fa fa-arrow-circle-right" ></i>
+                                </a>
                             </div>
                         </div>
+                        @endif
+                        @if($user_acr_to_report_count)
+                            <div class="col-lg-3 col-md-4 col-sm-12 ">
+                                <div class="card">
+                                    <div class="card-body ">
+                                        <div class="row">
+                                            <div class="col-5">
+                                                <p class="display-2 font-weight-bold">
+                                                {{$user_acr_to_report_count}}
+                                                </p>
+                                            </div>
+                                            <div class="col-7 d-flex align-items-center">
+                                                <p class="h4 text-center">At Reporting Officer</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="card-footer text-right text-decoration-none bg-light  " href="{{route('acr.others.index')}}">
+                                        <span>view</span>
+                                        <i class="fa fa-arrow-circle-right" ></i>
+                                    </a>
+                                </div>
+                            </div>
+                        @endif
+                        @if($user_acr_to_review_count)
+                            <div class="col-lg-3 col-md-4 col-sm-12 ">
+                                <div class="card">
+                                    <div class="card-body ">
+                                        <div class="row">
+                                            <div class="col-5">
+                                                <p class="display-2 font-weight-bold">
+                                                {{$user_acr_to_review_count}}
+                                                </p>
+                                            </div>
+                                            <div class="col-7 d-flex align-items-center">
+                                                <p class="h4 text-center">At Reviewing Officer</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="card-footer text-right text-decoration-none bg-light  " href="{{route('acr.others.index')}}">
+                                        <span>view</span>
+                                        <i class="fa fa-arrow-circle-right" ></i>
+                                    </a>
+                                </div>
+                            </div>
+                        @endif
+                        @if($user_acr_to_accept_count)
+                        <div class="col-lg-3 col-md-4 col-sm-12 ">
+                            <div class="card">
+                                <div class="card-body ">
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <p class="display-2 font-weight-bold">
+                                            {{$user_acr_to_accept_count}}
+                                            </p>
+                                        </div>
+                                        <div class="col-7 d-flex align-items-center">
+                                            <p class="h4 text-center">At Accepting Officer</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a class="card-footer text-right text-decoration-none bg-light  " href="{{route('acr.others.index')}}">
+                                    <span>view</span>
+                                    <i class="fa fa-arrow-circle-right" ></i>
+                                </a>
+                            </div>
+                        </div>
+                        @endif
                     </div>
                     @endif
                 </div>
