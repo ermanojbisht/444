621b2ca942ea66f6c30f7f398aa1835321a9a663
manojpanwar000 <manojpanwar000@yahoo.co.in>
2022-02-23 15:36:55 +0530

    m

--- resources/views/employee/home.blade.php
+++ resources/views/employee/home.blade.php
@@ -2,74 +2,78 @@
 @section('content')
 <div class="content">
     <div class="row">
+        @if(session('status'))
+            <div class="alert alert-info" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        @if(session('fail'))
+            <div class="alert alert-danger" role="alert">
+                {{ session('fail') }}
+            </div>
+        @endif
+        @if(session('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Dashboard
-                </div>
-
-                <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-info" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if(session('fail'))
-                        <div class="alert alert-danger" role="alert">
-                            {{ session('fail') }}
-                        </div>
-                    @endif
-                    @if(session('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-
-
-                    <p>
                     @if(auth()->user())
-                      &#128591; {{strtoupper(auth()->user()->shriName)}}
-                      @if(strpos(auth()->user()->email, 'emp.com') !== false )
-                      <p>For Security Resons .Please change your mail .</p>
-                      @if(file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php')))
-                        <a class="btn btn-info" href="{{ route('profile.password.edit') }}">
-                            <i class="fa-fw fas fa-key c-sidebar-nav-icon">
-                            </i>
-                            Change Your Email / Password
-                        </a>
-                      @endif
-                      @else
-                      @if(!auth()->user()->email_verified_at))
-                      <p>For Security Resons .Please verify your mail .</p>
-                        <a class="btn btn-danger" href="{{ route('verification.notice') }}">
-                            <i class="fa-fw fas fa-key c-sidebar-nav-icon">
-                            </i>
-                            Process Verification
-                        </a>
-                      @endif
-                      @endif
-                      {{-- @if(auth()->user()->chat_id <= 11111 )
-                      <br>Your telegram is not mapped with PWD System, You may like to share the info with set procedure .Consult to PWD IT Cell.  <a href="http://mis.pwduk.in/dms/index.php/summary/128-website-software-trainning-material/1276-subscribe-telegram-bot-for-pwd-alerts">You may follow this video link</a>
-                      @endif --}}
+                      <p>&#128591; {{strtoupper(auth()->user()->shriName)}}</p>
                     @endif
-                     @if(file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php')))
-                     <div>
-                        <a class="btn btn-success" href="{{ route('profile.password.edit') }}">
-                            <i class="fa-fw fas fa-key c-sidebar-nav-icon"> </i>
-                            Change Password
-                        </a>
-                     </div>
-                    @endif
-                    @if(file_exists(app_path('Http/Controllers/Auth/ChangeEmailController.php')))
-                        <div>
-                            <a class="btn btn-success" href="{{ route('profile.email.edit') }}">
-                                <i class="fa-fw fas fa-key c-sidebar-nav-icon"> </i>
-                                Change Email
-                            </a>
-                        </div>
-                    @endif
-                    </p>
+                </div>
+                <div class="card-body">
 
+                    @if(auth()->user())
+                        @if(strpos(auth()->user()->email, 'emp.com') !== false )
+                                @if(file_exists(app_path('Http/Controllers/Auth/ChangeEmailController.php')))
+                                    <div class="card border border-0">
+                                        <div class="col-auto border-left" >
+                                            <p class="h5"> For Security Resons .Please change your mail .</p>
+                                            <p>Please Use Your Personal Mail Only</p>
+                                            <a class="btn btn-danger" href="{{ route('profile.email.edit') }}">
+                                                <i class="fa fa-envelope"></i>
+                                                Change Your Email
+                                            </a>
+                                        </div>
+                                    </div>
+                              @endif
+                        @else
+                              @if(!auth()->user()->email_verified_at)
+                                <div class="card border border-0">
+                                    <div class="col-auto border-left" >
+                                        <p class="h5">Please verify your mail</p>
+                                        <a class="btn btn-danger" href="{{ route('verification.notice') }}">
+                                            <i class="fa fa-envelope"></i>
+                                            Process Verification
+                                        </a>
+                                    </div>
+                                </div>
+                              @endif
+                        @endif
+                    @endif
+                    @if(strpos(auth()->user()->email, 'emp.com') == false && auth()->user()->email_verified_at)
+                    <div class="row p-3">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-body ">
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <p class="display-3 font-weight-bold">
+                                            {{$user_acr_count}}
+                                            </p>
+                                        </div>
+                                        <div class="col-7 d-flex align-items-center">
+                                            <p class="h4 text-center">ACR Submitted</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
