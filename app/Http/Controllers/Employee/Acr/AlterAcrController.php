aba307cb24230d08673cdabf09ea7ec80895a0d0
ermanojbisht/data.pwduk.in <er_manojbisht@yahoo.com>
2022-02-28 13:18:12 +0530

    m

--- app/Http/Controllers/Employee/Acr/AlterAcrController.php
+++ app/Http/Controllers/Employee/Acr/AlterAcrController.php
@@ -66,7 +66,7 @@ class AlterAcrController extends Controller {
         //    make pdf  and mail notification
         dispatch(new MakeAcrPdfOnSubmit($acr, 'accept'));
 
-        return redirect()->back()->with('success', 'ACR Final Marks Updated');
+        return redirect()->route('employee.acr.view',['employee'=>$acr->employee_id])->with('success', 'ACR Final Marks Updated');
     }
 
     /**
