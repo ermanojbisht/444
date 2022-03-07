6794758e38446dd193fde437c56be19e08820afd
manojpanwar000 <manojpanwar000@yahoo.co.in>
2022-03-07 15:42:30 +0530

    employee report page UI

--- resources/views/employee/acr/employee_acr.blade.php
+++ resources/views/employee/acr/employee_acr.blade.php
@@ -1,103 +1,88 @@
 @extends('layouts.type200.main')
 
 @section('pagetitle')
-{{$employee->shriName }}'s ACR Details
-@endsection
-
-@section('breadcrumbNevigationButton')
-
+	{{$employee->shriName }}'s ACR Details
 @endsection
 
 @section('breadcrumb')
-@include('layouts._commonpartials._breadcrumb',
-[ 'datas'=> [
-['label'=> 'Home','route'=> 'employee.home', 'icon'=>'home', 'active'=>false],
-['label'=> 'My Acrs','active'=>true]]])
-@endsection
+	@include('layouts._commonpartials._breadcrumb',
+	[ 'datas'=> [
+	['label'=> 'Home','route'=> 'employee.home', 'icon'=>'home', 'active'=>false],
+	['label'=> 'My Acrs','active'=>true]]])
+	@endsection
 
 @section('content')
-<div class="card">
-	<div class="d-flex justify-content-end bg-transparent">
+<div class="card p-3">
+	<div class="d-flex justify-content-between ">
+		<p class="fw-bold h5"> Employee Code : {{$employee->id }} </p>
+		<p class="fw-bold h5"> Designation : {{$employee->designation->name }} </p>
 	</div>
 
-
-	<div class="row">
-		<div class="col-md-6">
-			<p class="fw-bold h5"> Employee Code : {{$employee->id }} </p>
-		</div>
-		<div class="col-md-6">
-			<p class="fw-bold h5"> Designation : {{$employee->designation->name }} </p>
-		</div>
-	</div>
-	<hr>
-
-
-	<table class="table mb-0 table-bordered ">
+	<table class="table mb-0 table-bordered " id="myTable">
 		<thead class="table-light fw-bold">
 			<tr class="align-middle text-center">
-				<th rowspan="2">#</th>
-				
-				<th colspan="2">Period</th>
-				<th rowspan="2">Status</th>
-				<th colspan="2">Reported</th>
-				<th colspan="2">Reviewed</th>
-				<th colspan="2">Accepted</th>
-				<th rowspan="2">Acr Id</th>
-				<th rowspan="2">analysis</th>
-			</tr>
-			<tr class="align-middle text-center">
-				<th>From </th>
-				<th>To </th>
-				<th>By </th>
-				<th>on Date </th>
-				<th>By </th>
-				<th>on Date </th>
-				<th>By </th>
-				<th>on Date </th>
-
+				<th>#</th>
+				<th>Year</th>
+				<th>From</th>
+				<th>Till</th>
+				<th>Status</th>
+				<th>Reported By</th>
+				<th>Reviewed By</th>
+				<th>Accepted By</th>
+				<th>Analysis</th>
+				<th>Action</th>
 			</tr>
 		</thead>
 		<tbody>
 			@foreach($acrs as $acr)
 			<tr class="{!! $acr->status_bg_color() !!}" style="--cui-bg-opacity: .25;">
 				<td>{{1+$loop->index }}</td>
-				
+				<td  class="align-middle">{{$acr->getFinancialYearAttribute()}}</td>
 				<td>{!! $acr->from_date->format('d&#160;M&#160;Y') !!}</td>
 				<td>{!! $acr->to_date->format('d&#160;M&#160;Y') !!}</td>
 				<td>{{$acr->status() }}</td>
 
 				@if(! $acr->is_active )
-				<td colspan="6">
-					{!! $acr->status() !!}
-				</td>
+					<td colspan="6">
+						{!! $acr->status() !!}
+					</td>
 				@else
 
-				<td>{{$acr->report_employee_id ? $acr->reportUser()->shriName : '' }} </td>
-				<td> @if ($acr->submitted_at)
-					{{ $acr->report_on ? Carbon\Carbon::parse($acr->report_on)->format('d M Y')
-					: 'Pending since ' .
-					Carbon\Carbon::parse(now())->diffInDays(Carbon\Carbon::parse($acr->submitted_at)) . ' days' }} @endif
+				<td>	
+					{{$acr->report_employee_id ? $acr->reportUser()->shriName : '' }} 
+					@if ($acr->submitted_at)
+						{{ $acr->report_on ? ' on '.Carbon\Carbon::parse($acr->report_on)->format('d M Y')
+						: ' Pending since '.Carbon\Carbon::parse(now())->diffInDays(Carbon\Carbon::parse($acr->submitted_at)).' days' }}
+					@endif
+				
 				</td>
-				<td>{{$acr->review_employee_id ? $acr->reviewUser()->shriName : '' }} </td>
-				<td> @if ($acr->report_on)
-					{{ $acr->review_on ? Carbon\Carbon::parse($acr->review_on)->format('d M Y') :
-					'Pending since ' . Carbon\Carbon::parse(now())->diffInDays(Carbon\Carbon::parse($acr->report_on)) . '
-					days' }} @endif
-				</td>
-				<td>{{$acr->accept_employee_id ? $acr->acceptUser()->shriName : '' }} </td>
-				<td>@if ($acr->review_on)
-					{{ $acr->accept_on ? Carbon\Carbon::parse($acr->accept_on)->format('d M Y') :
-					'Pending since ' . Carbon\Carbon::parse(now())->diffInDays(Carbon\Carbon::parse($acr->review_on)) .
-					' days' }} @endif </td>
 				<td>
+					{{$acr->review_employee_id ? $acr->reviewUser()->shriName : '' }} 
+					@if ($acr->report_on)
+						{{ $acr->review_on ? ' on '. Carbon\Carbon::parse($acr->review_on)->format('d M Y') :' Pending since ' . Carbon\Carbon::parse(now())->diffInDays(Carbon\Carbon::parse($acr->report_on)) . ' days' }} 
+					@endif
+				</td>
+				<td>
+					{{$acr->accept_employee_id ? $acr->acceptUser()->shriName : '' }}
+					@if ($acr->review_on) 
+						{{ $acr->accept_on ? ' on '.Carbon\Carbon::parse($acr->accept_on)->format('d M Y') : ' Pending since ' . Carbon\Carbon::parse(now())->diffInDays(Carbon\Carbon::parse($acr->review_on)) .' days' }} 
+					@endif 
+				</td>
+				<td>
+					@if($acr->missing >1)               
+                    wrong ( {{($acr->missing-1)}} ) days
+                    @else
+                    ok!
+                    @endif
+				</td>
+				<td class="d-flex d-flex justify-content-around">
 					@if(!$acr->submitted_at && !$acr->is_acknowledged)
 						@can('acknowledge-acr')
 							<form action="{{ route('acr.others.acknowledged') }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" >
                                 <input type="hidden" name="_method" value="POST">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <input type="hidden" name="acr_id" value="{{ $acr->id }}">
-                                <button type="submit" style="width:100%;" class="btn btn-danger "> Acknowledge ACR
-                                        </button>
+                                <button type="submit" class="btn btn-outline-danger btn-sm">Acknowledge</button>
                             </form>
 						@endcan
 					@endif
@@ -118,46 +103,21 @@
 								</a>
 							@endif
 				        @endcan
-					@else
-							--
 					@endif
-
 				</td>
 				@endif
-				<td>
-					@if($acr->missing >1)               
-                    wrong ( {{($acr->missing-1)}} ) days
-                    @else
-                    ok!
-                    @endif
-				</td>
-
-
 			</tr>
 			@endforeach
 		</tbody>
 	</table>
 </div>
-
 @endsection
-
-
-
 @section('footscripts')
-<script src="{{ asset('../plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script type="text/javascript">
-	$(document).ready(function () {
-        $("#success-alert").fadeTo(5000, 500).slideUp(500, function () {
-            $("#success-alert").slideUp(500);
-        });
-    });
-
-        document.addEventListener("DOMContentLoaded", function () {
-            // Datatables Responsive
-            $("#user_Request_Details").DataTable({
-                responsive: true
-            });
-        });
-</script>
-
+	<script src="{{ asset('../js/jquery.table.marge.js') }}"></script>
+	<script type="text/javascript">
+		$('#myTable').margetable({
+		  type: 2,
+		  colindex: [1]
+		});
+	</script>
 @endsection
