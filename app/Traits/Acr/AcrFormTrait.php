e318aa540547f715d7d31fc693b26fcffab4c7a0
ermanojbisht/data.pwduk.in <er_manojbisht@yahoo.com>
2022-06-16 17:10:09 +0530

    m

--- app/Traits/Acr/AcrFormTrait.php
+++ app/Traits/Acr/AcrFormTrait.php
@@ -18,6 +18,6 @@ trait AcrFormTrait
 
 	public function defineAcrGroup()
 	{
-		return ['ENC', 'CE', 'SE', 'EE', 'AE', 'AAE / JE', 'Others', 'Ministerial'];
+		return ['ENC', 'CE', 'SE', 'EE', 'AE', 'AAE / JE', 'Others', 'Ministerial/ PA/ Draftsman/ Ameen/ Work Agent/ Mate'];
 	}
 }
--- resources/views/employee/acr/view_part1.blade.php
+++ resources/views/employee/acr/view_part1.blade.php
@@ -6,6 +6,9 @@
 <p class="fw-semibold fs-5 text-center text-info my-0">PERFORMANCE APPRAISAL REPORT FOR
 	<span class="text-danger">
         {{$acr->typeName}}
+		@if(in_array($acr->acr_type_id,[32,33]))
+		PA/ Draftsman/ Ameen/ Work Agent/ Mate
+		@endif
 	</span>
 </p>
 <p>
