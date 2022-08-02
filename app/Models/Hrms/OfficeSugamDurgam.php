07794374ce67cd42ba518787cc7ff0daae2c480c
ermanojbisht/data.pwduk.in <er_manojbisht@yahoo.com>
2022-08-02 13:00:31 +0530

    m

--- app/Models/Hrms/OfficeSugamDurgam.php
+++ app/Models/Hrms/OfficeSugamDurgam.php
@@ -12,18 +12,6 @@ class OfficeSugamDurgam extends Model
     public $table = 'office_sugam_durgams';
     protected $dates = ['start_date', 'end_date', 'created_at', 'updated_at']; 
 
-    protected $fillable = [
-        'id',
-        'office_id',
-        'table_name',
-        'start_date',
-        'end_date',
-        'isdurgam',
-        'duration_factor',
-        'hr_office_id',
-        'created_at', 
-        'updated_at'
-    ];
-
+    protected $fillable = [ 'id','office_id','table_name','start_date','end_date','isdurgam','duration_factor','hr_office_id','created_at', 'updated_at'];
 }
  
